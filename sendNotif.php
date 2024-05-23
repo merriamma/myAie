bd5c6a331d2d3d795a8951341c8975a2addfa3a4
merriamma <163525938+merriamma@users.noreply.github.com>
2024-05-23 18:19:52 +0200

    Update sendNotif.php

--- sendNotif.php
+++ sendNotif.php
@@ -10,36 +10,25 @@ try {
     die('<p class="text-danger">Erreur de connexion à la base de données : ' . $e->getMessage() . '</p>');
 }
 
-if (isset($_POST['nom']) && isset($_POST['notification_name']) && isset($_POST['message'])) {
-    $nom = $_POST['nom'];
+if (isset($_POST['ensg']) && isset($_POST['notification_name']) && isset($_POST['message'])) {
+    $ensg = $_POST['ensg'];
     $notification_name = $_POST['notification_name'];
     $message = $_POST['message'];
-
-    // Récupérer l'ID du professeur à partir de son nom
-    $query = "SELECT nom FROM enseignant WHERE nom = ?";
-    $stmt = $myproject->prepare($query);
-    $stmt->execute([$nom]);
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    if ($row) {
-        $nomProf = $row['nom'];
-
+    foreach ($ensg as $ens) {
         // Insérer la notification
         $insert_notification = "INSERT INTO notification (notification_name, message, active) VALUES (?, ?, 1)";
         $stmt = $myproject->prepare($insert_notification);
         $stmt->execute([$notification_name, $message]);
         $notification_id = $myproject->lastInsertId();
 
-        // Lier la notification au professeur
+        // Lier la notification à chaque enseignant sélectionné
         $link_notification_prof = "INSERT INTO notification_enseignant (notification_id, nom_enseignant) VALUES (?, ?)";
         $stmt = $myproject->prepare($link_notification_prof);
-        $stmt->execute([$notification_id, $nomProf]);
-
-        header("Location: notifAdmin.php?success=1");
-        exit();
-    } else {
-        echo "No such teacher found.";
+        $stmt->execute([$notification_id, $ens]);
     }
+
+    header("Location: notifAdmin.php?success=1");
+    //     exit();
 } else {
     echo "Please fill in all fields.";
 }
